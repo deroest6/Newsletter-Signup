ed40c694954868645699bab407f4d618276762a6
Jantsen DeRoest <deroest6@hotmail.com>
2019-04-30 10:05:26 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+//jshint esversion:  6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+//Allows the use of static folders
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+
+  var data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName
+        }
+        }
+    ]
+  };
+
+  var jsonData = JSON.stringify(data);
+
+  var options = {
+    url: "https://us20.api.mailchimp.com/3.0/lists/97189cb54a",
+    method: "POST",
+    headers: {
+      "Authorization": "jantsen1 26cab0d1c2f560bf3ac65e01d4e633ec-us20"
+    },
+    body: jsonData
+  };
+
+  request(options, function(error, response, body) {
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if (response.statusCode === 200) {
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+  });
+
+
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000.");
+});
+
+// API Key
+// 26cab0d1c2f560bf3ac65e01d4e633ec-us20
+
+//List ID
+// 97189cb54a
